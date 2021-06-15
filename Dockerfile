66ce4153fe69a538ba6a50c705264de73050866c
Sungsoo <sungsahn0215@gmail.com>
2021-06-15 14:26:29 +0400

    guacamol without fcd

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,16 @@ RUN pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.7.0+c
 RUN pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.7.0+cu110.html
 RUN pip install torch-geometric
 
-RUN pip install guacamol
+RUN pip install joblib>=0.12.5
+RUN pip install numpy>=1.15.2
+RUN pip install scipy>=1.1.0
+RUN pip install tqdm>=4.26.0
+RUN pip install h5py==2.10.0
+RUN pip install flake8>=3.5.0
+RUN pip install mypy>=0.630
+RUN pip install pytest>=3.8.2
+RUN pip install guacamol --no-dependencies
+
 RUN pip install git+https://github.com/bp-kelley/descriptastorus
 RUN pip install chemprop
 RUN conda install -c conda-forge scikit-learn==0.21.3
--- model/ae.py
+++ model/ae.py
@@ -52,4 +52,36 @@ class RnnAutoEncoder(torch.nn.Module):
         x_seq, lengths, log_prob = self.decoder.sample(global_goal, vocab)
         return x_seq, lengths, log_prob
 
+class EducatedRnnAutoEncoder(RnnAutoEncoder):
+    def step(self, x_seq0, x_seq1, lengths0, lengths1):
+        batch_size = x_seq0.size(0)        
+        global_goal = self.global_goal.unsqueeze(0).expand(batch_size, -1)
+        goal0 = self.encoder(x_seq0, lengths0)
+        goal1 = self.encoder(x_seq1, lengths1)
         
+        global_logits0, logits0 = self.decoder(x_seq0[:, :-1], goal_list=[global_goal, goal1], lengths=lengths0-1)
+        global_logits1, logits1 = self.decoder(x_seq1[:, :-1], goal_list=[global_goal, goal0], lengths=lengths1-1)
+        
+        global_recon_loss = 0.5 * (
+            compute_rnn_ce(global_logits0, x_seq0[:, 1:], lengths0) 
+            + compute_rnn_ce(global_logits1, x_seq1[:, 1:], lengths1)
+        )
+        recon_loss = 0.5 * (
+            compute_rnn_ce(logits0, x_seq0[:, 1:], lengths0)
+            + compute_rnn_ce(logits1, x_seq1[:, 1:], lengths1)
+        )
+        elem_acc0, seq_acc0 = compute_rnn_accuracy(logits0, x_seq0[:, 1:], batch_size)
+        elem_acc1, seq_acc1 = compute_rnn_accuracy(logits1, x_seq1[:, 1:], batch_size)
+        elem_acc = 0.5 * (elem_acc0 + elem_acc1)
+        seq_acc = 0.5 * (seq_acc0 + seq_acc1)
+        loss = global_recon_loss + recon_loss
+
+        statistics = {
+            "loss/total": loss.item(),
+            "loss/global_recon": global_recon_loss.item(),
+            "loss/recon": recon_loss.item(),
+            "acc/recon/elem": elem_acc.item(),
+            "acc/recon/seq": seq_acc.item(),
+        }
+
+        return loss, statistics
