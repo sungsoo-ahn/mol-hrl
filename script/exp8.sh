464c7714d56dc6ff1e57c758cc91f045837c07fb
Sungsoo <sungsahn0215@gmail.com>
2021-07-27 10:52:47 +0400

    200 epochs

---
+++ script/exp8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CHECKPOINT_DIR="../resource/checkpoint"
+TAG="relational"
+
+python train.py \
+--autoencoder_type relational \
+--checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
+--tag $TAG
+
+python eval.py --checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" --tag $TAG
