d7c6d8e99efdfa67fe8a482a22f1a4313499b8e3
Sungsoo <sungsahn0215@gmail.com>
2021-07-29 19:40:13 +0400

    tmp

--- script/exp8.sh
+++ script/exp8.sh
@@ -3,10 +3,10 @@
 CHECKPOINT_DIR="../resource/checkpoint"
 TAG="relational"
 
-#python train.py \
-#--autoencoder_type relational \
-#--checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
-#--tag $TAG
+python train.py \
+--autoencoder_type relational \
+--checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
+--tag $TAG
 
 python eval.py --checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" --tag "${TAG}_linear"
 python eval.py --checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" --tag $TAG
--- src/module/pl_autoencoder.py
+++ src/module/pl_autoencoder.py
@@ -76,7 +76,7 @@ class AutoEncoderModule(pl.LightningModule):
         parser.add_argument("--graph_encoder_num_layers", type=int, default=5)
 
         # SequentialEncoder specific
-        parser.add_argument("--sequence_encoder_hidden_dim", type=int, default=1024)
+        parser.add_argument("--sequence_encoder_hidden_dim", type=int, default=2048)
         parser.add_argument("--sequence_encoder_num_layers", type=int, default=3)
 
         # SequentialDecoder specific
