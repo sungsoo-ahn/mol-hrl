6f592ef54416b8e5bf8e6f5f5ea1ad02f17eb355
Sungsoo <sungsahn0215@gmail.com>
2021-08-03 16:25:56 +0400

    tmp

--- script/exp3.sh
+++ script/exp3.sh
@@ -1,58 +1,9 @@
 #!/bin/bash
 
 CHECKPOINT_DIR="../resource/checkpoint"
-TAG="nopretrain"
+TAG="rgroup"
 
-python run_conddecoder.py \
---train_split train_01 \
---score_func_name logp \
---checkpoint_path "${CHECKPOINT_DIR}/${TAG}_01_logp.pth" \
---tag "${TAG}_01_logp"
-
-python run_conddecoder.py \
---train_split train_01 \
---score_func_name molwt \
---checkpoint_path "${CHECKPOINT_DIR}/${TAG}_01_molwt.pth" \
---tag "${TAG}_01_molwt"
-
-python run_conddecoder.py \
---train_split train_01 \
---score_func_name qed \
---checkpoint_path "${CHECKPOINT_DIR}/${TAG}_01_qed.pth" \
---tag "${TAG}_01_qed"
-
-python run_conddecoder.py \
---train_split train_10 \
---score_func_name logp \
---checkpoint_path "${CHECKPOINT_DIR}/${TAG}_10_logp.pth" \
---tag "${TAG}_10_logp"
-
-python run_conddecoder.py \
---train_split train_10 \
---score_func_name molwt \
---checkpoint_path "${CHECKPOINT_DIR}/${TAG}_10_molwt.pth" \
---tag "${TAG}_10_molwt"
-
-python run_conddecoder.py \
---train_split train_10 \
---score_func_name qed \
---checkpoint_path "${CHECKPOINT_DIR}/${TAG}_10_qed.pth" \
---tag "${TAG}_10_qed"
-
-python run_conddecoder.py \
---train_split train \
---score_func_name logp \
---checkpoint_path "${CHECKPOINT_DIR}/${TAG}_00_logp.pth" \
---tag "${TAG}_00_logp"
-
-python run_conddecoder.py \
---train_split train \
---score_func_name molwt \
---checkpoint_path "${CHECKPOINT_DIR}/${TAG}_00_molwt.pth" \
---tag "${TAG}_00_molwt"
-
-python run_conddecoder.py \
---train_split train \
---score_func_name qed \
---checkpoint_path "${CHECKPOINT_DIR}/${TAG}_00_qed.pth" \
---tag "${TAG}_00_qed"
+python run_autoencoder.py \
+--checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
+--input_rgroup \
+--tag $TAG
--- script/exp4.sh
+++ script/exp4.sh
@@ -1,37 +1,72 @@
-#! /bin/bash
+#!/bin/bash
 
 CHECKPOINT_DIR="../resource/checkpoint"
-TAG="nopretrain"
+TAG="fragment"
+
+python run_autoencoder.py \
+--checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
+--input_fragment \
+--tag $TAG
 
 python run_conddecoder.py \
 --train_split train_01 \
---score_func_name penalized_logp \
+--score_func_name logp \
+--load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
 --checkpoint_path "${CHECKPOINT_DIR}/${TAG}_01_logp.pth" \
---tag "${TAG}_01_plogp"
+--tag "${TAG}_01_logp"
 
-python run_condopt.py \
+python run_conddecoder.py \
 --train_split train_01 \
---load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}_01_logp.pth" \
---tag "${TAG}_01_plogp"
+--score_func_name molwt \
+--load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
+--checkpoint_path "${CHECKPOINT_DIR}/${TAG}_01_molwt.pth" \
+--tag "${TAG}_01_molwt"
+
+python run_conddecoder.py \
+--train_split train_01 \
+--score_func_name qed \
+--load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
+--checkpoint_path "${CHECKPOINT_DIR}/${TAG}_01_qed.pth" \
+--tag "${TAG}_01_qed"
 
 python run_conddecoder.py \
 --train_split train_10 \
---score_func_name penalized_logp \
+--score_func_name logp \
+--load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
 --checkpoint_path "${CHECKPOINT_DIR}/${TAG}_10_logp.pth" \
---tag "${TAG}_10_plogp"
+--tag "${TAG}_10_logp"
 
-python run_condopt.py \
+python run_conddecoder.py \
 --train_split train_10 \
---load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}_10_logp.pth" \
---tag "${TAG}_10_plogp"
+--score_func_name molwt \
+--load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
+--checkpoint_path "${CHECKPOINT_DIR}/${TAG}_10_molwt.pth" \
+--tag "${TAG}_10_molwt"
+
+python run_conddecoder.py \
+--train_split train_10 \
+--score_func_name qed \
+--load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
+--checkpoint_path "${CHECKPOINT_DIR}/${TAG}_10_qed.pth" \
+--tag "${TAG}_10_qed"
 
 python run_conddecoder.py \
 --train_split train \
---score_func_name penalized_logp \
+--score_func_name logp \
+--load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
 --checkpoint_path "${CHECKPOINT_DIR}/${TAG}_00_logp.pth" \
---tag "${TAG}_00_plogp"
+--tag "${TAG}_00_logp"
 
-python run_condopt.py \
+python run_conddecoder.py \
 --train_split train \
---load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}_00_logp.pth" \
---tag "${TAG}_00_plogp"
+--score_func_name molwt \
+--load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
+--checkpoint_path "${CHECKPOINT_DIR}/${TAG}_00_molwt.pth" \
+--tag "${TAG}_00_molwt"
+
+python run_conddecoder.py \
+--train_split train \
+--score_func_name qed \
+--load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
+--checkpoint_path "${CHECKPOINT_DIR}/${TAG}_00_qed.pth" \
+--tag "${TAG}_00_qed"
