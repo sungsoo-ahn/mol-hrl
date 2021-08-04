0a72794bc510975a79167c43a00c0efe4aa86fc1
Sungsoo <sungsahn0215@gmail.com>
2021-08-04 15:56:54 +0400

    tmp

--- src/run_conddecoder.py
+++ src/run_conddecoder.py
@@ -18,10 +18,6 @@ if __name__ == "__main__":
     parser.add_argument("--tags", type=str, nargs="+")
     hparams = parser.parse_args()
 
-    if hparams.load_checkpoint_path != "":
-        hparams.max_epochs = 10
-        hparams.check_val_every_n_epoch = 1
-
     neptune_logger = NeptuneLogger(
         project_name="sungsahn0215/molrep", experiment_name="run_conddecoder", params=vars(hparams),
     )
