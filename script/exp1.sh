c3262cbba4e7b396988ac3b07d14060176759d55
Sungsoo <sungsahn0215@gmail.com>
2021-08-04 17:06:57 +0400

    tmp

--- script/exp1.sh
+++ script/exp1.sh
@@ -5,7 +5,6 @@ TAG="fragment"
 
 python run_condopt.py \
 --train_split train_256 \
---score_func_name penalized_logp \
 --cond_embedding_mlp \
 --load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
 --tags $TAG 256 penalized_logp
