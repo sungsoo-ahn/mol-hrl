6a9a7eb04a300208230ee77b489ef62375e0ac30
Sungsoo <sungsahn0215@gmail.com>
2021-08-04 15:42:18 +0400

    tmp

--- script/exp4.sh
+++ script/exp4.sh
@@ -12,5 +12,4 @@ python run_conddecoder.py \
 --train_split train_256 \
 --score_func_name logp \
 --cond_embedding_mlp \
---load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
 --tags $TAG 256 logp
