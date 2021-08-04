dcbefc28a77ff9502cf7116b6ff6e70b77489d90
Sungsoo <sungsahn0215@gmail.com>
2021-08-04 20:11:05 +0400

    tmp

--- script/exp2.sh
+++ script/exp2.sh
@@ -3,9 +3,23 @@
 CHECKPOINT_DIR="../resource/checkpoint"
 TAG="base"
 
+#python run_conddecoder.py \
+#--train_split train_256 \
+#--score_func_name logp \
+#--cond_embedding_mlp \
+#--load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
+#--tags $TAG 256 logp
+
 python run_conddecoder.py \
 --train_split train_256 \
---score_func_name logp \
+--score_func_name molwt \
 --cond_embedding_mlp \
 --load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
---tags $TAG 256 logp
+--tags $TAG 256 molwt
+
+python run_conddecoder.py \
+--train_split train_256 \
+--score_func_name qed \
+--cond_embedding_mlp \
+--load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
+--tags $TAG 256 qed
