b648b9e30c17ff7b7c053b0ad75c51ee003de865
Sungsoo <sungsahn0215@gmail.com>
2021-08-11 15:20:49 +0400

    tmp

--- script/exp0.sh
+++ script/exp0.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
 
 CHECKPOINT_DIR="../resource/checkpoint"
-TAG="fragment"
+TAG="base"
 
-#python run_conddecoder.py \
-#--train_split train_256 \
-#--score_func_name logp \
-#--cond_embedding_mlp \
-#--load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
-#--tags $TAG 256 logp
-
-python run_conddecoder.py \
---train_split train_256 \
---score_func_name molwt \
---cond_embedding_mlp \
+python run_plug.py \
+--train_split train \
+--score_func_name logp \
+--plug_beta 0.1 \
+--plug_code_dim 256 \
 --load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
---tags $TAG 256 molwt
-
-python run_conddecoder.py \
---train_split train_256 \
---score_func_name qed \
---cond_embedding_mlp \
---load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
---tags $TAG 256 qed
+--tags $TAG 256 logp largebeta largedim
--- script/exp1.sh
+++ script/exp1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 CHECKPOINT_DIR="../resource/checkpoint"
-TAG="fragment"
+TAG="base"
 
-python run_condopt.py \
---train_split train_256 \
---decoder_max_length 81 \
---cond_embedding_mlp \
+python run_plug.py \
+--train_split train \
+--score_func_name logp \
+--plug_beta 0.1 \
 --load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
---tags $TAG 256 penalized_logp
+--tags $TAG 256 logp largebeta
