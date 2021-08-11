59559f039f013e2d8e9f7ded3101f5383352d2db
Sungsoo <sungsahn0215@gmail.com>
2021-08-11 21:36:32 +0400

    tmp

--- script/exp5.sh
+++ script/exp5.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
 
 CHECKPOINT_DIR="../resource/checkpoint"
-TAG="nopretrain"
+TAG="base"
 
-python run_conddecoder.py \
+python run_plug.py \
 --train_split train \
 --score_func_name logp \
---cond_embedding_mlp \
---max_epochs 100 \
---check_val_every_n_epoch 10 \
---tags $TAG full logp
-
-python run_conddecoder.py \
---train_split train \
---score_func_name molwt \
---cond_embedding_mlp \
---max_epochs 100 \
---check_val_every_n_epoch 10 \
---tags $TAG full molwt
-
-python run_conddecoder.py \
---train_split train \
---score_func_name qed \
---cond_embedding_mlp \
---max_epochs 100 \
---check_val_every_n_epoch 10 \
---tags $TAG full qed
+--plug_depth 4 \
+--plug_width_factor 1.0 \
+--load_checkpoint_path "${CHECKPOINT_DIR}/${TAG}.pth" \
+--tags $TAG 4 1.0
