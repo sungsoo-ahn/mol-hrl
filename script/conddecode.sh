37db3ed0baf7a1809529f98be7d45dc5a9f5311f
Sungsoo <sungsahn0215@gmail.com>
2021-08-26 12:31:36 +0400

    tmp

--- script/conddecode.sh
+++ script/conddecode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python conddecoder.py --decoder_hidden_dim 1024 --tags hidden1024
+python run_conddecoder.py --decoder_hidden_dim 1024 --tags hidden1024
 
-python conddecoder.py --decoder_hidden_dim 512 --tags hidden512
+python run_conddecoder.py --decoder_hidden_dim 512 --tags hidden512
 
-python conddecoder.py --decoder_hidden_dim 256 --tags hidden256
+python run_conddecoder.py --decoder_hidden_dim 256 --tags hidden256
