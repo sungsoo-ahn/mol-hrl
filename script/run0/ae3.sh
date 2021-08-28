a83645f8e952a12ab9a286b3b6e18b5ab173329d
Sungsoo <sungsahn0215@gmail.com>
2021-08-29 00:06:38 +0400

    tmp

---
+++ script/run0/ae3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python run0_autoencoder.py --code_dim 256 --tag ae_code256_frag --dataset_name fraggraph2seq
---
+++ script/run0/vqae3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python run0_vqautoencoder.py --vq_code_dim 256 --tag vqae_code256_fraggraph --dataset_name fraggraph2seq
